9aae643f3bf79d5f0cd712b6c6646a37b3b7b5d3
Ch1ck3nNeedsRNG <chick_is_bored>
2025-04-26 05:54:19 -0400

    fix selecting wrong shader

--- java/src/main/kotlin/com/perseuspotter/apelles/depression/ChromaShader.kt
+++ java/src/main/kotlin/com/perseuspotter/apelles/depression/ChromaShader.kt
@@ -9,7 +9,7 @@ open class ChromaShader(fragSrc: String?, vertSrc: String?, private val twoD: Bo
         val CHROMA_3D_TEX = ChromaShader(getResource("/shaders/chroma_3d_tex.fsh"), getResource("/shaders/chroma_3d_tex.vsh"), false)
         val CHROMA_2D = ChromaShader(getResource("/shaders/chroma_2d.fsh"), getResource("/shaders/chroma_2d.vsh"), true)
         val CHROMA_2D_TEX = ChromaShader(getResource("/shaders/chroma_2d_tex.fsh"), getResource("/shaders/chroma_2d_tex.vsh"), true)
-        fun get(num: Int, tex: Boolean) = if (tex) (if (num == 1) CHROMA_2D else CHROMA_3D) else (if (num == 1) CHROMA_2D_TEX else CHROMA_3D_TEX)
+        fun get(num: Int, tex: Boolean) = if (tex) (if (num == 1) CHROMA_2D_TEX else CHROMA_3D_TEX) else (if (num == 1) CHROMA_2D else CHROMA_3D)
     }
 
     open fun updateUniforms(pt: Double, t: Int) {
