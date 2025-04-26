88ba257852b1482c82452b4f1d144fe4586251cc
Ch1ck3nNeedsRNG <chick_is_bored>
2025-04-26 05:49:37 -0400

    remove unused vao

--- java/src/main/kotlin/com/perseuspotter/apelles/depression/VAO.kt
+++ java/src/main/kotlin/com/perseuspotter/apelles/depression/VAO.kt
@@ -8,7 +8,6 @@ import java.nio.FloatBuffer
 import java.nio.IntBuffer
 
 class VAO(val vertexCount: Int, val indicesCount: Int, val hasC: Boolean, val hasN: Boolean, val hasT: Boolean, val mode: Int) {
-    // var vaoId: Int = -1
     var vboPId: Int = -1
     var iboId: Int = -1
     val MAX_VERTEX_COUNT = vertexCount * 3 / 2
@@ -66,37 +65,6 @@ class VAO(val vertexCount: Int, val indicesCount: Int, val hasC: Boolean, val ha
 
         GL15.glGenBuffers(b)
         iboId = b[0]
-
-        /*
-        vaoId = GL30.glGenVertexArrays()
-        GL30.glBindVertexArray(vaoId)
-
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboPId)
-        GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0)
-        GL20.glEnableVertexAttribArray(0)
-
-        if (hasC) {
-            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboCId)
-            GL20.glVertexAttribPointer(1, 4, GL11.GL_FLOAT, false, 0, 0)
-            GL20.glEnableVertexAttribArray(1)
-        } // else GL20.glDisableVertexAttribArray(1)
-
-        if (hasN) {
-            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboNId)
-            GL20.glVertexAttribPointer(2, 3, GL11.GL_FLOAT, false, 0, 0)
-            GL20.glEnableVertexAttribArray(2)
-        } // else GL20.glDisableVertexAttribArray(2)
-
-        if (hasT) {
-            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboTId)
-            GL20.glVertexAttribPointer(3, 2, GL11.GL_FLOAT, false, 0, 0)
-            GL20.glEnableVertexAttribArray(3)
-        } // else GL20.glDisableVertexAttribArray(3)
-
-        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, iboId)
-
-        GL30.glBindVertexArray(0)
-        */
     }
 
     fun update() {
@@ -115,8 +83,6 @@ class VAO(val vertexCount: Int, val indicesCount: Int, val hasC: Boolean, val ha
 
     fun draw() {
         if (!hasMeaningfulData) return
-        // VAOShader.get(hasC, hasN, hasT).bind()
-        // GL30.glBindVertexArray(vaoId)
         GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboPId)
         GL11.glVertexPointer(
             3,
@@ -148,6 +114,5 @@ class VAO(val vertexCount: Int, val indicesCount: Int, val hasC: Boolean, val ha
     fun destroy() {
         GL15.glDeleteBuffers(vboPId)
         GL15.glDeleteBuffers(iboId)
-        // GL30.glDeleteVertexArrays(vaoId)
     }
 }
