550dc32c4f1d2c33336049edfad5b1c26a784501
Ch1ck3nNeedsRNG <chick_is_bored>
2025-05-17 04:03:38 -0400

    add compat command

--- index.js
+++ index.js
@@ -641,3 +641,4 @@ export function createPerFrameOutliner(tester, color, width, { phase = false, ch
 // require('./test');
 
 register('command', () => Client.scheduleTask(20, () => Java.type('com.perseuspotter.apelles.outline.EntityOutlineRenderer').dump = true)).setName('apellesdumpfbo', true);
+register('command', t => Client.scheduleTask(() => APRenderer.USE_NEW_SHIT = !!t)).setName('apellesusecompat', true);
