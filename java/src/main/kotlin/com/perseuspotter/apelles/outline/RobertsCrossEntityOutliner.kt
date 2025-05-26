67485a0743cb8262b37393eb1973dfa9e421beed
Ch1ck3nNeedsRNG <chick_is_bored>
2025-05-26 06:11:38 -0400

    add roberts cross

---
+++ java/src/main/kotlin/com/perseuspotter/apelles/outline/RobertsCrossEntityOutliner.kt
@@ -0,0 +1,5 @@
+package com.perseuspotter.apelles.outline
+
+import com.perseuspotter.apelles.outline.shader.robertscross.RobertsCrossRender
+
+object RobertsCrossEntityOutliner : KernelEntityOutliner(2, "RobertsCross", RobertsCrossRender)
