706ff28eb52c8e61cec7c9691ac997c5bd748b44
Ch1ck3nNeedsRNG <chick_is_bored>
2025-05-26 18:38:56 -0400

    close handle on jar in case updates with game open

--- IHATERHINO.js
+++ IHATERHINO.js
@@ -1,2 +1,7 @@
 import { ___________________________________shhh } from './index';
 ___________________________________shhh();
+
+const JSContextFactory = com.chattriggers.ctjs.engine.langs.js.JSContextFactory;
+const classLoaderF = JSContextFactory.class.getDeclaredField('classLoader');
+classLoaderF.setAccessible(true);
+classLoaderF.get(JSContextFactory.INSTANCE).close();
