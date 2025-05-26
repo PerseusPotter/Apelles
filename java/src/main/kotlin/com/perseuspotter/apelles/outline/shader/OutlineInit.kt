4a8e902ed0c490f8c0241d86adc53dc9c9021213
Ch1ck3nNeedsRNG <chick_is_bored>
2025-05-26 05:45:38 -0400

    generalize sobelInit

--- java/src/main/kotlin/com/perseuspotter/apelles/outline/shader/OutlineInit.kt
+++ java/src/main/kotlin/com/perseuspotter/apelles/outline/shader/OutlineInit.kt
@@ -1,10 +1,10 @@
-package com.perseuspotter.apelles.outline.shader.sobel
+package com.perseuspotter.apelles.outline.shader
 
 import com.perseuspotter.apelles.depression.Shader
 import com.perseuspotter.apelles.state.Color
 import org.lwjgl.opengl.GL20
 
-object SobelInit : Shader(getResource("/shaders/sobel/sobelInit.frag"), getResource("/shaders/sobel/sobelInit.vert")) {
+object OutlineInit : Shader(getResource("/shaders/outlineInit.frag"), getResource("/shaders/outlineInit.vert")) {
     fun setColor(col: Color) {
         GL20.glUniform4f(getUniformLoc("color"), col.r, col.g, col.b, col.a)
     }
