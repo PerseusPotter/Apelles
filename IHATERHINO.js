c08741ac8d7f8bd02df330973d51a9477eca707f
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2025-05-31 00:55:57 -0400

    offset in case goofy mod

--- IHATERHINO.js
+++ IHATERHINO.js
@@ -1,7 +1,9 @@
 import { ___________________________________shhh } from './index';
 ___________________________________shhh();
 
-const JSContextFactory = com.chattriggers.ctjs.engine.langs.js.JSContextFactory;
-const classLoaderF = JSContextFactory.class.getDeclaredField('classLoader');
-classLoaderF.setAccessible(true);
-classLoaderF.get(JSContextFactory.INSTANCE).close();
+Client.scheduleTask(100, () => {
+  const JSContextFactory = com.chattriggers.ctjs.engine.langs.js.JSContextFactory;
+  const classLoaderF = JSContextFactory.class.getDeclaredField('classLoader');
+  classLoaderF.setAccessible(true);
+  classLoaderF.get(JSContextFactory.INSTANCE).close();
+});
