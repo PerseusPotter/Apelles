8ac2725dc43795b8def2dacf6e735a730229550a
Ch1ck3nNeedsRNG <chick_is_bored>
2025-06-03 17:41:43 -0400

    add a constants file in case the 2 people want to use it

---
+++ CONSTANTS.js
@@ -0,0 +1,31 @@
+export const AP_LIGHTING_NONE = 0;
+export const AP_LIGHTING_SMOOTH = 1;
+export const AP_LIGHTING_FLAT = 2;
+
+export const AP_CHROMA_NONE = 0;
+export const AP_CHROMA_2D = 1;
+export const AP_CHROMA_3D = 2;
+
+export const AP_TEXT_BLACK_BOX_NONE = 0;
+export const AP_TEXT_BLACK_BOX_WHOLE = 1;
+export const AP_TEXT_BLACK_BOX_PER_LINE = 2;
+
+export const AP_TEXT_ANCHOR_TOP_LEFT = 0b00;
+export const AP_TEXT_ANCHOR_MIDDLE_LEFT = 0b01;
+export const AP_TEXT_ANCHOR_BOTTOM_LEFT = 0b10;
+export const AP_TEXT_ANCHOR_TOP_MIDDLE = 0b100;
+export const AP_TEXT_ANCHOR_MIDDLE_MIDDLE = 0b101;
+export const AP_TEXT_ANCHOR_BOTTOM_MIDDLE = 0b110;
+export const AP_TEXT_ANCHOR_TOP_RIGHT = 0b1000;
+export const AP_TEXT_ANCHOR_MIDDLE_RIGHT = 0b1001;
+export const AP_TEXT_ANCHOR_BOTTOM_RIGHT = 0b1010;
+
+export const AP_TEXT_ALIGNX_LEFT = 0;
+export const AP_TEXT_ALIGNX_RIGHT = 1;
+export const AP_TEXT_ALIGNX_CENTER = 2;
+
+export const AP_TEXT_PARSE_MODE_NONE = 0;
+export const AP_TEXT_PARSE_MODE_SECTION = 1;
+export const AP_TEXT_PARSE_MODE_SECTION_IGNORE = 2;
+export const AP_TEXT_PARSE_MODE_ALL = 3;
+export const AP_TEXT_PARSE_MODE_ALL_IGNORE = 4;
