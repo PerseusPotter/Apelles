49cf9e62dca3942d22c1a83378ffe9f0e45f071a
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2025-06-03 04:00:33 -0400

    a

--- java/src/main/kotlin/com/perseuspotter/apelles/state/Color.kt
+++ java/src/main/kotlin/com/perseuspotter/apelles/state/Color.kt
@@ -18,6 +18,7 @@ data class Color @JvmOverloads constructor(
     )
     constructor(arr: FloatArray) : this(arr[0], arr[1], arr[2], arr.getOrElse(3) { 1f })
     constructor(arr: List<Double>) : this(arr[0].toFloat(), arr[1].toFloat(), arr[2].toFloat(), arr.getOrElse(3) { 1f }.toFloat())
+    constructor(r: Double, g: Double, b: Double, a: Double) : this(r.toFloat(), g.toFloat(), b.toFloat(), a.toFloat())
 
     override fun toString(): String = "($r, $g, $b, $a)"
 }
