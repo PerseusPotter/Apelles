818fefd894d825574e87095077cd0353fb848d3f
Ch1ck3nNeedsRNG <chick_is_bored>
2025-06-08 07:12:08 -0400

    bad

--- IHATERHINO.js
+++ IHATERHINO.js
@@ -1,9 +1,2 @@
 import { ___________________________________shhh } from './index';
 ___________________________________shhh();
-
-register('gameUnload', () => {
-  const JSContextFactory = com.chattriggers.ctjs.engine.langs.js.JSContextFactory;
-  const classLoaderF = JSContextFactory.class.getDeclaredField('classLoader');
-  classLoaderF.setAccessible(true);
-  classLoaderF.get(JSContextFactory.INSTANCE).close();
-});
